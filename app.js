79ec4ab2be10f10a2ab2907ea3fd6b046884dcf5
paul-github <paul86677889@gmail.com>
2022-08-05 11:38:25 +0800

    get all file in folder

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 // require csvtojson module
 const CSVToJSON = require('csvtojson');
 const fs = require('fs');
-const filename='sample';
-// convert users.csv file to JSON array
-CSVToJSON().fromFile('csv/'+filename+'.csv')
-    .then(file => {
-        // users is a JSON array
-        // write JSON array to file
-         fs.writeFile('json/'+filename+'.json', JSON.stringify(file, null, 4), (err) => {
-            if (err) {
-               throw err;
-             }
-          });
-    }).catch(err => {
-        // log error if any
-        console.log(err);
-    });
+const path = require('path')
+
+const csvFolder = __dirname+'/csv/';
+fs.readdirSync(csvFolder).forEach(csvfile => {
+  let extension = path.extname(csvfile);
+  console.log('extension',extension)
+  let filename = path.basename(csvfile, extension);
+  if(extension!=='.csv'){
+    return
+  }
+  CSVToJSON().fromFile('csv/'+csvfile)
+  .then(file => {
+      // users is a JSON array
+      // write JSON array to file
+       fs.writeFile('json/'+filename+'.json', JSON.stringify(file, null, 4), (err) => {
+          if (err) {
+             throw err;
+           }
+        });
+  }).catch(err => {
+      // log error if any
+      console.log(err);
+  });
+});
